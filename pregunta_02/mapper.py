3439bde8dfc32e10a5df8b7939875115abcaee6b
Brenda Rincon Rico <101733090+brincon1@users.noreply.github.com>
2022-06-05 15:34:39 -0500

    primer intento 2da pregunta

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,32 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    #
+    # itera sobre cada linea de codigo recibida
+    # a traves del flujo de entrada
+    #
+   
+    #for line in sys.stdin:
+    for row in sys.stdin:
+        dividir = row.split(",")
+        segundaCol = dividir[3]
+        terceraCol = dividir[4]
+        
+        
+        #print(segundaCol)
+
+        #
+        # genera las tuplas palabra \tabulador 1
+        # ya que es un conteo de palabras
+        #
+    
+
+        #
+        # escribe al flujo estandar de salida
+    
+
+        sys.stdout.write(f"{segundaCol}\t {terceraCol}\n")
